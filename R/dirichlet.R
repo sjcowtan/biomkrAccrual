7e8c9630d0610da11098707b2f0ce1385ee78bdd
S J Cowtan <umsco@leeds.ac.uk>
2024-07-22 19:52:22 +0100

    Assertions on rdirichlet_alt() input

--- R/dirichlet.R
+++ R/dirichlet.R
@@ -1,8 +1,34 @@
-#'
+#' Generate a matrix of `n` rows of sets of probabilities 
+#' generated from the Dirichlet distribution, each row 
+#' summing to 1. Uses the alternative $\mu$, $\phi$ 
+#' parameterisation for the Dirichlet distribution, 
+#' representing means and precision.
 #' 
+#' @param n Number of sets of probabilities (defaults to 1)
+#' @param mu Vector of mean values for each probability in the set
+#' @param phi Parameter representing precision, where precision is 
+#' 1/variance. Must be positive.
+#'
+#' @example rdirichlet_alt(3, c(0.001, 0.029, 0.7), 4)
+rdirichlet_alt <- function(n = 1, mu = c(0.2, 0.3, 0.5), phi = 1) {
 
-rdirichlet_alt <- function(n = 1, phi = 1, mu) {
+  ### Check and convert inputs
 
-  
+  # True if n is "close to an integer"
+  checkmate::assert_integerish(n, lower = 1, len = 1)
+
+  # mu should be a numeric vector in the range (0, 1)
+  checkmate::assert_vector(
+    mu, min.len = 2, strict = TRUE, any.missing = FALSE
+  )
+  checkmate::assert_numeric(
+    mu, lower = 10^-10, upper = 1 - 10^-10
+  )
+
+  # phi should be a positive number
+  checkmate::assert_number(phi, lower = 10^-10)
+
+  # Make n actually an integer
+  n <- as.integer(n)
 
 }
--- tests/testthat/test-dirichlet.R
+++ tests/testthat/test-dirichlet.R
@@ -1,15 +1,37 @@
-# Testing rdirichlet_alt
+### Testing rdirichlet_alt
+
 n <- 3
 phi <- 2
 mu <- c(0.01, 0.1, 0.29, 0.6)
 
-dirichlet_output <- rdirichlet_alt(n, phi, mu)
+# Incomplete testing of assertions on input
+
+test_that("Error if n is not integerish", {
+  expect_error(rdirichlet_alt(0.5, mu, phi))
+})
+
+test_that("Error if mu is not a vector", {
+  expect_error(rdirichlet_alt(n, as.matrix(mu, nrow = 4), phi))
+})
+
+test_that("Error if mu is not between 0 and 1", {
+  expect_error(rdirichlet_alt(n, c(0.02, 1), phi))
+})
+
+test_that("Error if n is not a numeric scalar greater than 0", {
+  expect_error(rdirichlet_alt(n, mu, 0))
+})
+
+
+# Testing of successful function execution
+
+dirichlet_output <- rdirichlet_alt(n, mu, phi)
 
 test_that("Output of rdirichlet_alt is a matrix", {
   expect_s3_class(dirichlet_output, "matrix")
 })
 
-testthat("Output of rdirichlet_alt is floating point", {
+test_that("Output of rdirichlet_alt is floating point", {
   expect_type(rdirichlet_alt, "double")
 })
 
@@ -17,11 +39,22 @@ test_that("Matrix dimensions from rdirichlet_alt are correct", {
   expect_equal(dim(dirichlet_output), c(n, length(mu)))
 })
 
-test_that("Values from rdirichlet_alt are in range (0, 1)", {
-  expect_gt(range(x)[1], 0)
-  expect_lt(range(x)[2], 1)
+test_that("No missing values in output from rdirichlet_alt", {
+  expect_equal(sum(is.na(dirichlet_output), 0))
+})
+
+test_that("Values from rdirichlet_alt are greater than 0", {
+  expect_gt(range(dirichlet_output)[1], 0)
+})
+
+test_that("Values from rdirichlet_alt are less than 1", {
+  expect_lt(range(dirichlet_output)[2], 1)
 })
 
 test_that("Matrix rows from rdirichlet_alt sum to 1", {
   expect_equal(rowSums(dirichlet_output), rep(1, n))
+})
+
+test_that("Matrix rows from rdirichlet_alt sum to 2", {
+  expect_equal(rowSums(dirichlet_output), rep(2, n))
 })
