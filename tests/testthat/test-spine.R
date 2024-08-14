3747c99392a50388cc8aeca6f471fade06d4f1e0
S J Cowtan <umsco@leeds.ac.uk>
2024-08-14 16:54:04 +0100

    Update test-spine.R for new output format

--- tests/testthat/test-spine.R
+++ tests/testthat/test-spine.R
@@ -1,11 +1,5 @@
 spine_out <- spine()
 
-test_that("Output of spine() should be a list of length 2", {
-  checkmate::expect_list(spine_out, len = 2)
-})
-
-test_that("Second element of spine_output should be an integer vector", {
-  checkmate::expect_atomic_vector(
-    spine_out[[2]], names = "named", min.len = 2
-  )
+test_that("Output of spine() should be an array", {
+  checkmate::expect_array(spine_out)
 })
