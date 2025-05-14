fe97a4891d7d4c87933edf82219b87d1cb2eda89
S J Cowtan <umsco@leeds.ac.uk>
2025-05-14 18:07:51 +0100

    Testing do_choose_cap to make sure doesn't return more of any element than exist in original population

--- tests/testthat/test-recruit.R
+++ tests/testthat/test-recruit.R
@@ -54,9 +54,43 @@ test_that("Can sum by treatment a 3-D accrual array", {
   )
 })
 
-test_that("Output correct from treat_sums for valid input", {
+test_that("Output correct from treat_sums for valid array", {
   expect_identical(
     treat_sums(arr),
     as.integer(c(84, 100, 116))
+  )
+})
+
+## Testing treat_sums on an accrual object
+
+test_that("Output correct from treat_sums for valid accrual object", {
+  expect_identical(
+    treat_sums(acc_obj),
+    as.integer(c(0, 0, 0))
+  )
+})
+
+## Testing capping
+uncapped <- c(1, 1, 2, 5, 8)
+capped <- do_choose_cap(uncapped, 3)
+
+test_that("do_choose_cap caps population correctly", {
+  expect_identical(
+    length(capped),
+    as.integer(3)
+  )
+})
+
+# Need to be able to compare frequencies for elements in capped only
+table_uncapped <- as.data.frame(table(uncapped))
+table_capped <- as.data.frame(table(capped))
+
+test_that("do_choose_caps does not produce extra repeats", {
+  expect_true(
+    all(
+      table_uncapped[
+        which(table_uncapped$uncapped %in% table_capped$capped),
+      ]$Freq >= table_capped$Freq
+    )
   )
 })
