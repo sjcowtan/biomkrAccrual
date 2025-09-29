9a4a8d5e6c3171e02e4aeb02e7011ffc3e8aec08
S J Cowtan <umsco@leeds.ac.uk>
2025-09-29 14:19:40 +0100

    tests for writing output files (spine.R)

--- tests/testthat/test-spine.R
+++ tests/testthat/test-spine.R
@@ -1,4 +1,9 @@
-bma_out <- biomkrAccrual(quietly = TRUE)
+output_path <- "../test_biomkrAccrual_output_data/"
+
+bma_out <- biomkrAccrual(
+  quietly = TRUE, 
+  output_path = output_path
+)
 
 test_that("Output of biomkrAccrual() should be an S7 object", {
   checkmate::expect_class(bma_out, "S7_object")
@@ -10,34 +15,46 @@ test_that("Property accrual of output of biomkrAccrual() should be an array", {
 
 test_that("Output directory exists or has been created", {
   checkmate::expect_directory_exists(
-    "../biomkrAccrual_output_data/",
+    output_path,
     access = "rwx"
   )
 })
 
 test_that("Figures directory exists or has been created", {
   checkmate::expect_directory_exists(
-    "../biomkrAccrual_output_data/figures/",
+    paste0(output_path, "figures/"),
     access = "rwx"
   )
 })
 
 # Test that data and figure files exist with today's date in test data area
-date <- format(Sys.time(), "%y-%m-%d")
-print(getwd())
+date <- format(Sys.time(), "%Y-%m-%d")
 
-## Can't put wildcard in test
-#files <- list.files(
-#  "../biomkrAccrual_output_data/",
-#  pattern = "accrual-", date, "_*\\.csv"
-#)
-#print(files)
-
-#test_that("spine: writes accrual CSV file", {
-#  checkmate::expect_file_exists(
-#    paste0("../biomkrAccrual_output_data/accrual-", date, "_*.csv")
-#  )
-#})
+test_that("spine: writes accrual CSV file", {
+  checkmate::expect_file_exists(
+    file.path(
+      substr(output_path, 1, nchar(output_path) - 1),
+      list.files(
+        path = substr(output_path, 1, nchar(output_path) - 1), 
+        pattern = paste0("^accrual-", date, "-.*\\.csv$")
+      )
+    )
+  )
+})
 
 
-# Clean up by deleting the data and figure files with today's date
+test_that("spine: writes at least one PNG file", {
+  checkmate::expect_file_exists(
+    file.path(
+      paste0(output_path, "figures"),
+      list.files(
+        path = paste0(output_path, "figures"), 
+        pattern = paste0("^accrual-", date, "-.*\\.png$")
+      )
+    )
+  )
+})
+
+
+# Clean up by deleting the test data and figure files
+unlink(output_path, recursive = TRUE)
