6e81bdf2090d9d468fa1cbaaed9a1eb1d43db908
S J Cowtan <umsco@leeds.ac.uk>
2025-10-07 14:52:37 +0100

    Tests for basic-utils.R

--- tests/testthat/test-basic-utils.R
+++ tests/testthat/test-basic-utils.R
@@ -1,9 +1,12 @@
 # Testing makeifnot_dir
 
+# Put test directory in the test code output folder
 testpath <- "../biomkrAccrual_output_data"
-if (dir.exists("../biomkrAccrual_output_data")) {
-  test_path <- paste0(testpath, "/makeadir")
+if (!dir.exists(testpath)) {
+  dir.create(testpath, mode = "0777")
 }
+
+testpath <- paste0(testpath, "/makeadir")
 
 test_that("Non-existant directory can be created", {
   expect_no_failure(
@@ -15,3 +18,22 @@ test_that("Non-existant directory can be created", {
   )
 })
 
+test_that("Exits correctly if directory already exists", {
+  expect_no_failure(
+    makeifnot_dir(testpath)
+  )
+})
+
+# Make a non-writeable directory and test can't create directory in it
+dir.create(paste0(testpath, "/nonwriteable"), mode = "0555")
+
+bad_testpath <- paste0(testpath, "/nonwriteable/bad")
+
+test_that("Fails if parent directory is not writeable", {
+  expect_error(
+    makeifnot_dir(bad_testpath)
+  )
+})
+
+# Clean up
+unlink(testpath, recursive = TRUE)
