a4cf0a9060995bd4d5d7a95a2be0e112f97fb5c6
S J Cowtan <umsco@leeds.ac.uk>
2025-10-08 14:29:45 +0100

    tests for accrual_to_long()

--- tests/testthat/test-utils-graphics.R
+++ tests/testthat/test-utils-graphics.R
@@ -25,4 +25,80 @@ test_that("get_base_family returns either Arial or sans", {
 })
 
 
-# Testing 
+# Testing get_arm_closures
+
+# Testing accrual_plot_from_file
+
+# Testing accrual_to_long
+
+acc_df <- data.frame(
+  T1 = rep(3, 12),
+  T2 = rep(5, 12),
+  C = rep(8, 12)
+)
+
+test_that("accrual_to_long: returns dataframe of correct format", {
+  checkmate::expect_data_frame(
+    accrual_to_long(acc_df),
+    types = c("integerish", "factor"),
+    any.missing = FALSE,
+    null.ok = FALSE,
+    ncols = 3,
+    nrows = 36,
+    col.names = "named"
+  )
+})
+
+atl_out <- accrual_to_long(acc_df)
+
+test_that("accrual_to_long: correct column names", {
+  checkmate::expect_set_equal(
+    names(atl_out),
+    c("Week", "Arm", "Recruitment"),
+    ordered = TRUE
+  )
+})
+
+test_that("accrual_to_long: Arm is a factor", {
+  checkmate::expect_factor(
+    atl_out$Arm,
+    levels = c("C", "T1", "T2"),
+    empty.levels.ok = FALSE,
+    null.ok = FALSE
+  )
+})
+
+test_that("accrual_to_long: Week and Recruitment are integerish", {
+  checkmate::expect_integerish(
+    atl_out$Week
+  )
+  checkmate::expect_integerish(
+    atl_out$Recruitment
+  )
+})
+
+test_that("accrual_to_long: Recruitment is cumulatively summed", {
+  expect_equal(
+    atl_out$Recruitment,
+    c(
+      seq.int(from = 3, length.out = 12, by = 3),
+      seq.int(from = 5, length.out = 12, by = 5),
+      seq.int(from = 8, length.out = 12, by = 8)
+    )
+  )
+})
+
+test_that("accrual_to_long: Weeks are consecutive starting at 1", {
+  expect_equal(
+    atl_out$Week,
+    rep(1:12, 3)
+  )
+})
+
+test_that("accrual_to_long: arm names in expected distribution", {
+  expect_equal(
+    as.character(atl_out$Arm),
+    rep(c("T1", "T2", "C"), each = 12)
+  )
+})
+
