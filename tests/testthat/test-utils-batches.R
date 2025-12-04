f0d5139d0a388d9a29196ad798266a89e36e5008
S J Cowtan <umsco@leeds.ac.uk>
2025-12-04 13:01:50 +0000

    Daily commit

---
+++ tests/testthat/test-utils-batches.R
@@ -0,0 +1,15 @@
+# Testing the code for running multiple simulations
+
+biomkrAccrualSim(
+  n = 500,
+  target_arm_size = 20,
+  target_interim = 10,
+  target_control = 60,
+  shared_control = TRUE,
+  accrual_period = 4,
+  interim_period = 2,
+  fixed_centre_starts = TRUE,
+  quietly = TRUE,
+  keep_files = TRUE
+)
+
