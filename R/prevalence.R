5368d1532017951ac3ffbe3b29ece4e6f71cb339
S J Cowtan <umsco@leeds.ac.uk>
2026-03-05 17:18:37 +0000

    remove cat statement

--- R/prevalence.R
+++ R/prevalence.R
@@ -258,8 +258,6 @@ get_recruit_arm_prevalence <- function(
     ncol = ncol(props_df) - 1
   )
 
-  cat(as.vector(region_prevalence))
-
   checkmate::assert_numeric(
     region_prevalence,
     lower = 0,
